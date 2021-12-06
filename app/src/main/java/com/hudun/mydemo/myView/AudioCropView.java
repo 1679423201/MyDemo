fb8e9a8dd752a3bbe3bf707a4a1b9d09be52e5bf
YMD <yangmeiduan@hudunsoft.com>
2021-12-06 11:42:16 +0800

    添加了剪辑条功能

--- app/src/main/java/com/hudun/mydemo/myView/AudioCropView.java
+++ app/src/main/java/com/hudun/mydemo/myView/AudioCropView.java
@@ -5,13 +5,13 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -29,57 +29,46 @@ import com.hudun.mydemo.R;
  * </pre>
  */
 public class AudioCropView extends View {
+    private int MOVE_NULL = 0;  //不移动
+    private int MOVE_LEFT = 1;  //移动左边
+    private int MOVE_RIGHT = 2; //移动右边
+    private int MOVE_ALL = 3;   //移动全部==不移动
+    private int frag = 0; //手指按下位置，变量为以上4种
+
     private Paint mPaint;
     private Bitmap mAudioWaveBitmap;    //波形图
     private int mWidth; //视图宽度
     private int mHeight;    //视图高度
     private Rect movableRect; //可移动矩形
     private int movableRectBackgroundColor = 0xfffbfafa; //背景颜色
-    Rect mSrcRect;  //定义图像大小的矩形
-    Rect mDestRect; //定义图像位置的矩形
-    private Bitmap bitmapMain;
+    private Rect mSrcRect;  //定义图像大小的矩形
+    private Rect mDestRect; //定义图像位置的矩形
+    private Bitmap mPlayingCursorBitmap;//指示器
+    private Rect controlDestRect; //控制指示器位置的Rect
+    private float startTime = 0f;    //开始时间
+    private float finishTime = 100f;   //结束时间
+    private float currentTime = 0f;  //当前时间
 
-    public AudioCropView(Context context) {
-        super(context);
-        init(null);
-    }
+    private static final String TAG = "VIEW_TEXT";
 
-    public AudioCropView(Context context, @Nullable AttributeSet attrs) {
-        this(context,null,0);
-        init(attrs);
-    }
-
-    public AudioCropView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        init(attrs);
-    }
 
     private void init(@Nullable AttributeSet attrs){
             //从资源中获取Bitmap对象
-            bitmapMain = BitmapFactory.decodeResource(getResources(), R.drawable.ic_audio_wave);
+            Bitmap bitmapMain = BitmapFactory.decodeResource(getResources(), R.drawable.ic_audio_wave);
+            mPlayingCursorBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ic_playing_cursor);
             mAudioWaveBitmap = Bitmap.createBitmap(bitmapMain.getWidth() + 10, bitmapMain.getHeight() *2
                     , bitmapMain.getConfig());
             Canvas canvas = new Canvas(mAudioWaveBitmap);
             mSrcRect = new Rect(0,0,bitmapMain.getWidth(),bitmapMain.getHeight());
-            mDestRect = new Rect(0,mAudioWaveBitmap.getHeight()/4,bitmapMain.getWidth(),mAudioWaveBitmap.getHeight()/4*3);
+            mDestRect = new Rect(5,mAudioWaveBitmap.getHeight()/4,bitmapMain.getWidth(),mAudioWaveBitmap.getHeight()/4*3);
             canvas.drawBitmap(bitmapMain, mSrcRect, mDestRect, null);
             bitmapMain.recycle();
             mPaint = new Paint();
+            controlDestRect = new Rect();
     }
 
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        drawBackground(canvas);
-    }
 
-    @Override
-    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-        mWidth = w;
-        mHeight = h;
-        resizeMovableRect();
-    }
 
     /***
      * 绘制图形背景
@@ -93,10 +82,10 @@ public class AudioCropView extends View {
         Paint nPaint = new Paint();
         nPaint.setColorFilter(new PorterDuffColorFilter(Color.RED, PorterDuff.Mode.SRC_IN));
         nPaint.setStyle(Paint.Style.FILL);
-        canvas.drawRect(0, movableRect.top, mWidth, movableRect.bottom,mPaint);
+        canvas.drawRect(0, 0, mWidth, mHeight,mPaint);
         canvas.drawBitmap(mAudioWaveBitmap
                 , new Rect(5,0,mAudioWaveBitmap.getWidth(),mAudioWaveBitmap.getHeight())
-                , movableRect, nPaint);
+                , new Rect(5,0,mWidth, mHeight), nPaint);
     }
 
     /***
@@ -104,7 +93,8 @@ public class AudioCropView extends View {
      * @param canvas
      */
     public void drawPlayingCursor(Canvas canvas){
-
+        Rect srcRect = new Rect(0, 0, mPlayingCursorBitmap.getWidth(), mPlayingCursorBitmap.getHeight());
+        canvas.drawBitmap(mPlayingCursorBitmap, srcRect, controlDestRect, null);
     }
 
     /***
@@ -112,6 +102,121 @@ public class AudioCropView extends View {
      */
     void resizeMovableRect(){
         movableRect = new Rect(0,0,mWidth,mHeight);
+    }
+
+    /***
+     * todo:1.播放条自动播放 √
+     * todo:2.添加剪辑条 √
+     * todo:3.剪辑条可调节 √
+     * -2021-12-06
+     */
+
+
+    /***
+     * 播放条开始播放
+     */
+    public void startPlaying(float time){
+        controlDestRect.left = (int) (time/finishTime * mWidth);
+        controlDestRect.right = (int) (time/finishTime * mWidth) + mPlayingCursorBitmap.getWidth();
+        postInvalidate();
+    }
+
+    /***
+     * 设置剪辑进度条
+     * @param canvas
+     */
+    public void setClipBar(Canvas canvas){
+        mPaint = new Paint();
+        mPaint.setColor(Color.GREEN);
+        mPaint.setAlpha(99);
+        canvas.drawRect(movableRect.left+30, 0, movableRect.left+50, mHeight, mPaint);
+        mPaint.setColor(Color.GREEN);
+        canvas.drawCircle(movableRect.left+40, 40, 40, mPaint);
+        mPaint.setAlpha(99);
+        canvas.drawRect(movableRect.right-60, 0, movableRect.right-40, mHeight, mPaint);
+        mPaint.setColor(Color.GREEN);
+        canvas.drawCircle(movableRect.right-50, mHeight-40, 40, mPaint);
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: ");
+        float x = event.getRawX();
+        float y = event.getRawY();
+        Log.d(TAG, "onTouchEvent: x == " +x);
+        Log.d(TAG, "onTouchEvent: y == " +y);
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:{
+                frag = isMove(x,y); //记录手指按下位置
+                break;
+            }
+            case MotionEvent.ACTION_MOVE:{
+                Log.d(TAG, "onTouchEvent:  === " + isMove(x,y));
+                if(frag== 0) break;
+                if(frag == 3) break;
+                if(frag == 1){
+                    movableRect.left = (int) x - 30;
+                }else if(frag == 2){
+                    movableRect.right = (int) x + 50;
+                }
+                invalidate();
+                break;
+            }
+            case MotionEvent.ACTION_UP:break;
+            default:break;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Log.d(TAG, " onDraw: ");
+        drawBackground(canvas);
+        drawPlayingCursor(canvas);
+        setClipBar(canvas);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        Log.d(TAG, " onChange: ");
+        mWidth = w;
+        mHeight = h;
+        resizeMovableRect();
+        controlDestRect = new Rect(0, 0, mPlayingCursorBitmap.getWidth(), mHeight);
+    }
+    public AudioCropView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public AudioCropView(Context context, @Nullable AttributeSet attrs) {
+        this(context,attrs,0);
+        init(attrs);
+    }
+
+    public AudioCropView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    int isMove(float x, float y){
+        float xLeft = (x-(movableRect.left+30));
+        float xRight = (x-(movableRect.right-50));
+        float Y = (y*y);
+        if(xLeft > -20 && xLeft < 20){
+            if(xRight > -20 && xRight < 20){
+                return 3;
+            }
+            return 1;
+        }
+        if(xRight > -20 && xRight < 20){
+            return 2;
+        }
+        return 0;
+    }
+    public interface ControlPlaying{
+        void startPlaying();
     }
 }
 
--- app/src/main/java/com/hudun/mydemo/myView/MyViewTextActivity.java
+++ app/src/main/java/com/hudun/mydemo/myView/MyViewTextActivity.java
@@ -3,14 +3,79 @@ package com.hudun.mydemo.myView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import com.hudun.mydemo.R;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MyViewTextActivity extends AppCompatActivity {
+
+    private AudioCropView audioCropView;
+    private static final String TAG = "VIEW_TEXT";
+    float time = 0;
+    private Timer timer = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_view_text);
+        audioCropView = findViewById(R.id.mAudioView);
+        if(audioCropView == null){
+            Log.d(TAG, "onCreate:  View为空");
+        }else {
+            Log.d(TAG, "onCreate: View 不是空");
+        }
+
     }
+
+    private boolean frag = false;
+    Object object = new Object();
+    TimerTask timerTask = new TimerTask() {
+        @Override
+        public void run() {
+            synchronized (object){
+                Log.d(TAG, "run: ++"+time);
+                if(frag){
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                else {
+                    audioCropView.startPlaying(time);
+                    time += 0.1f;
+                    Log.d(TAG, "run: " + time);
+                }
+            }
+        }
+    };
+    public void startPlay(View view) {
+        if(timer == null){
+            timer = new Timer("播放定时");
+            timer.scheduleAtFixedRate(timerTask, 100,100);
+        }else {
+            synchronized (object){
+                frag = false;
+                object.notifyAll();
+            }
+        }
+
+    }
+
+
+    public void stopPlay(View view) {
+        frag = true;
+    }
+    @Override
+    protected void onDestroy() {
+        if(timer != null){
+            timer.cancel();
+        }
+        super.onDestroy();
+    }
+
 }
