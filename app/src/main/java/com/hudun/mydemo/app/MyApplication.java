0e9b58afe9db1402bb2bff3ef7d5aaf86f54ffd9
YMD <yangmeiduan@hudunsoft.com>
2021-12-14 18:42:46 +0800

    增加了音乐查找了动态权限申请功能

---
+++ app/src/main/java/com/hudun/mydemo/app/MyApplication.java
@@ -0,0 +1,11 @@
+package com.hudun.mydemo.app;/**
+ * <pre>
+ *      @ClassName MyApplication
+ *      @Author  :YMD
+ *      @E-mail  :1679423201@qq.com
+ *      @Date 2021/12/14 16:40
+ *      @Desc    :
+ *      @Version :1.0
+ * </pre>
+ */public class MyApplication {
+}
---
+++ app/src/main/java/com/hudun/mydemo/untils/AudioUntil.java
@@ -0,0 +1,11 @@
+package com.hudun.mydemo.untils;/**
+ * <pre>
+ *      @ClassName AudioUntil
+ *      @Author  :YMD
+ *      @E-mail  :1679423201@qq.com
+ *      @Date 2021/12/14 15:51
+ *      @Desc    :
+ *      @Version :1.0
+ * </pre>
+ */public class AudioUntil {
+}
